0e44fbda69d41898787dd4ae6083c2c363601fb2
FalcovdH <falco.vandenhoogen@gmail.com>
2024-02-27 20:26:09 +0100

    ok

--- src/main.py
+++ src/main.py
@@ -1,4 +1,17 @@
-import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
+from pathlib import Path
+from loguru import logger
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-print("Falco")
+
+
+processed = Path("../data/processed")
+datafile = processed / "whatsapp-20240213-202158.csv"
+if not datafile.exists():
+    logger.warning("Datafile does not exist. First run src/preprocess.py, and check the timestamp!")
+
+df = pd.read_csv(datafile)
+print(df.dtypes)
