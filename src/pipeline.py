daf46f39c0f8a4ad1260e21a28e62476c58cb20a
FalcovdH <falco.vandenhoogen@gmail.com>
2024-02-28 08:13:31 +0100

    PDM update

---
+++ src/pipeline.py
@@ -0,0 +1,19 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from pathlib import Path
+from loguru import logger
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+
+processed = Path(r"C:/Users/a427617/Documents/Master Data science/Blok 3 - Data mining/test-pdm/data/processed/")
+print(processed)
+datafile = processed / "whatsapp-20240214-112323.parq"
+print(datafile)
+if not datafile.exists():
+    logger.warning("Datafile does not exist. First run src/preprocess.py, and check the timestamp!")
+
+df = pd.read_parquet(datafile)
+print(df.dtypes)
